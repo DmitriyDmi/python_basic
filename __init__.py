f0fb6a776b0a97626336f5b225edf36e38f41560
DmitriyDmi <120082173+DmitriyDmi@users.noreply.github.com>
2024-05-10 09:21:14 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .task1 import rename_files
+from .file4 import generate_files
+from .main import generate_number_file
+from .s2 import generate_name_file
+from .s3 import process_files
+from .s5 import generate_with_dictionary
+from .s6 import sort_files
